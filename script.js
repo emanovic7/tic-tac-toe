c47d2b27c3cbde2c187f3b1143813abf795b1453
github username <github email address>
2018-10-18 21:50:15 -0400

    add some functions to script, call turn in index.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Define turns
+let totalMoves = 0;
 let turn = 0;
 let currentGame = 0;
 
@@ -6,33 +7,101 @@ let currentGame = 0;
 const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
+//Row & Column Counts
+
+let rowCount = [
+  [0,0,0],
+  [0,0,0],
+  [0,0,0]
+]
+
+let columnCount = [
+  [0,0,0],
+  [0,0,0],
+  [0,0,0]
+]
+
+let diagonalCount = [
+  [0,0,0],
+  [0,0,0],
+  [0,0,0]
+]
+
 //define rows
 const rows = Array.from(document.getElementsByClassName('box'));
 
 //define clicks
-
 let clicked = [
   [false, false, false],
   [false, false, false],
   [false, false, false]
 ]
 
+
+//Player Turn
+
+function playerTurn(){
+  turn = turn === 0 ? 1 : 0
+  turnDiv.innerHTML = `Player ${currentChip()} it's your turn!`
+}
+
 // select boxes
+const boxes = Array.from(document.getElementsByClassName('box'))
+
+boxes.forEach(box =>
+  box.addEventListener('click', selectBox())
+)
 
 function selectBox(e){
   const x = parseInt(this.dataset.x)
   const y = parseInt(this.dataset.y)
   if (clicked[x][y]) return
+
+  editHTML(this)
+  updateGame(x,y)
 }
+
+function updateGame(x,y){
+  clicked[x][y] = true
+  gameCheck(x,y)
+}
+
+
+function currentPlay(){
+  return turn === 0 ? 'O' : 'X'
+}
+
+function editHTML(e){
+  e.innerHTML = currentPlay();
+}
+
+function gameCheck(x,y){
+  totalMoves++;
+  rowCount[x][turn]++;
+  columnCount[y][turn]++;
+  diagonalCheck(x,y)
+}
+
+function diagonalCheck(x,y){
+  if (y===x) diagonalCount[0][turn]++
+  if ((y === 2 && x === 0) ||
+            (y === 1 && x === 1) ||
+            (y === 0 && x === 2)) diagonalCount[1][turn]++
+}
+
 
 
 
 
 //Reset Game
-document.getElementById('Reset').addEventListener('click', resetGame)
+document.getElementById('Reset').addEventListener('click',
+  function resetGame(){
+    document.querySelectorAll('td').empty()
+    turn = 0;
+    currentGame = 0;
+});
 
-function resetGame(){
-  document.querySelectorAll('td').empty()
-  turn = 0;
-  currentGame = 0;
-}
+//Save Game
+document.getElementById('Save Game').addEventListener('click', saveGame(){
+
+})
